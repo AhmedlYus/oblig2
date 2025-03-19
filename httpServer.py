6278f45a0339009634550f8a78711cdf08daae35
Columnaia <Ahmyus2@outlook.com>
2025-03-19 19:39:29 +0100

    git server and client.

---
+++ httpServer.py
@@ -0,0 +1,52 @@
+from socket import *
+import sys
+
+#local host config for server
+host = 'localhost'
+port = 8080
+
+#creating a TCP socket for the Http. 
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((host, port))
+serverSocket.listen(1)
+# 2 Web server is created
+# 2.1 is consolidated into this task, where the httpClient runs on TCP
+print(f"server running on {host}:{port} and listening")
+
+while True:
+    connection, addr = serverSocket.accept()
+    print(f"Connection from {addr}")
+
+    try:
+        message = connection.recv(1024).decode()
+        if not message:
+            connection.close()
+            continue
+
+        print("recived: \n", message)
+
+        filename = message.split()[1]
+
+        f = open(filename[1:])
+        output = f.read()
+        header = "HTTP/1.1 200 OK\r\n"
+        packet = header.encode() + output.encode()
+        connection.sendall(packet)
+
+        #this loop sends each letter seperately. 
+        #for i in range(0, len(output)):
+        #    connection.send(output[i].encode())
+        #    connection.send("\r\n".encode())
+
+        connection.close()
+    except IOError:     #if no file is found it will return an 404 not found response with an html code.
+        connection.send("HTTP/1.1 404 Not Found \r\n\r\n".encode())
+        connection.send("<html><head><body><h1>404 Not Found</h1></body></head></html>")
+    
+    # after data is sent the connection closes until the server is restarted and called again.    
+    finally:
+        connection.close()
+    serverSocket.close()
+    sys.exit()
+
+
---
+++ webClient.py
@@ -0,0 +1,29 @@
+from socket import *
+import sys
+import argparse
+
+#https://docs.python.org/3/library/argparse.html these are the commandline arguments.
+parse = argparse.ArgumentParser(prog="Client")
+parse.add_argument('-i', '--server', type=str)
+parse.add_argument('-p', '--port', type=int)
+parse.add_argument('-f', '--file', type=str)
+arg = parse.parse_args()
+
+# function for the tcp client that will communicate with the server
+def client(server, port, file):
+    # we create an socket that will handle TCP connection and connect to it through the host and prot
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((server, port))
+    #this sends a get request for the file from the server. 
+    request = f"GET /{file} HTTP/1.1\r\nHost: {server}\r\n\r\n " . format(arg.file, arg.server)
+    clientSocket.send(request.encode())
+
+    res = clientSocket.recv(4096)
+
+    print(res.decode())
+    
+    clientSocket.close()
+
+client(arg.server, arg.port, arg.file)
+
+
